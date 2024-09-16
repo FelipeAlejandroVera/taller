aa470104e76d9e2f84958a76518184b02cdf3caa
felipe <123>
2024-09-15 21:58:41 -0300

    prueba 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        taller 1
+        taller 12
         
         </p>
       </header>
